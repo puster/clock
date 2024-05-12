d0c6bc517c58f2837d92d07b8423fa0665878fec
puster <puster@gmail.com>
2024-05-12 21:57:05 +0800

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,10 +1,14 @@
-input.onButtonPressed(Button.A, function () {
+input.onButtonPressed(Button.AB, function () {
     kitronik_halo_hd.setTime(17, 38, 0)
 })
 let haloDisplay = kitronik_halo_hd.createZIPHaloDisplay(60)
 haloDisplay.setBrightness(10)
 basic.forever(function () {
     haloDisplay.clear()
+    haloDisplay.setBrightness(10)
+    for (let j = 0; j <= kitronik_halo_hd.readTimeForZip(TimeParameter.Seconds); j++) {
+        haloDisplay.setZipLedColor(j, kitronik_halo_hd.colors(ZipLedColors.Blue))
+    }
     haloDisplay.setBrightness(255)
     for (let index = 0; index <= 11; index++) {
         haloDisplay.setZipLedColor(5 * index, kitronik_halo_hd.colors(ZipLedColors.Red))
@@ -12,11 +16,10 @@ basic.forever(function () {
     haloDisplay.setBrightness(10)
     haloDisplay.setZipLedColor(kitronik_halo_hd.readTimeForZip(TimeParameter.Hours), kitronik_halo_hd.colors(ZipLedColors.Green))
     haloDisplay.setZipLedColor(kitronik_halo_hd.readTimeForZip(TimeParameter.Minutes), kitronik_halo_hd.colors(ZipLedColors.White))
-    haloDisplay.setZipLedColor(kitronik_halo_hd.readTimeForZip(TimeParameter.Seconds), kitronik_halo_hd.colors(ZipLedColors.Blue))
     haloDisplay.show()
     basic.pause(1000)
 })
 basic.forever(function () {
     basic.showString(kitronik_halo_hd.readTime().substr(0, 5))
-    basic.pause(2000)
+    basic.pause(1000)
 })
